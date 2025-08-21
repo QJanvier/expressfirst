5ce117f0a6d75ba3b4fd479d909f737168672718
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-08-21 16:40:54 +0200

    put req

--- src/app.js
+++ src/app.js
@@ -59,3 +59,16 @@ app.get('/api/products', (req, res) => {
     id:123, name: 'chicken breast', price: 12
   }])
 })
+
+
+app.put('/api/users/:id', (req, res) => {
+  const { body, params: { id } } = req
+  const parsedID = parseInt(id)
+  if (isNaN(parsedID)) 
+    return res.sendStatus(400)
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedID)
+  if (findUserIndex === -1)
+    return res,sendStatus(404)
+  mockUsers[findUserIndex] = { id: parsedID, ...body }
+  return res.sendStatus(200)
+})
