2a4a460620f9515c6877964b0cb520c0cb75aef9
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-08-26 14:22:45 +0200

    cookies

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const routes = require('../src/routes/index')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 app.use(express.json())
 //middleware to parse JSON bodies
+app.use(cookieParser("mySecret"))
 app.use(routes)
 
 const PORT =  process.env.PORT || 3000
@@ -11,6 +13,6 @@ const PORT =  process.env.PORT || 3000
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
 
 app.get('/',  (req, res) =>{
+  res.cookie('Pedro', 'express', { maxAge: 60000, signed: true })
   res.status(201).send({msg: 'Hello World'})
-  res.cookie('name', 'express', { domain: 'localhost', path: '/' })
 })
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 
 router.get('/api/products', (req, res) => {
-  res.send([{
-    id:123, name: 'chicken breast', price: 12
-  }])
+  console.log(req.cookies)
+  console.log(req.signedCookies.Pedro)
+  if (req.signedCookies.Pedro && req.signedCookies.Pedro === 'express')
+    return res.send([{ id:123, name: 'chicken breast', price: 12 }])
+    return res.send({ msg: "Sorry you need the correct cookie" })
 })
 
 module.exports = router
