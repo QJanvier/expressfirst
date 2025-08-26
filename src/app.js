dc0f77b9b84304d2ea3bd05cf0faa4ae1bfd189c
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-08-26 15:45:46 +0200

    session express

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const routes = require('../src/routes/index')
 const cookieParser = require('cookie-parser')
+const session = require('express-session')
 const app = express()
 
 app.use(express.json())
 //middleware to parse JSON bodies
 app.use(cookieParser("mySecret"))
+app.use(session(
+  {
+    secret: "mySecret",
+    saveUninitialized: false,
+    resave: false,
+    cookie: { maxAge: 60000 * 60 }}
+))
 app.use(routes)
 
 const PORT =  process.env.PORT || 3000
@@ -13,6 +21,9 @@ const PORT =  process.env.PORT || 3000
 app.listen(PORT, console.log(`Server running on port ${PORT}`))
 
 app.get('/',  (req, res) =>{
+  console.log(req.session)
+  console.log(req.sessionID)
+  req.session.visited = true
   res.cookie('Pedro', 'express', { maxAge: 60000, signed: true })
   res.status(201).send({msg: 'Hello World'})
 })
--- src/routes/user.js
+++ src/routes/user.js
@@ -11,15 +11,23 @@ router.get('/api/users',
         .withMessage('Filter must be a non-empty string')
         .isLength({ min: 3, max: 10 })
         .withMessage('Filter must be between 3 and 10 characters')
-        , (req, res) => {
-      const result = validationResult(req)
-      console.log(result)
-      const { query: { filter, value } } = req
-    
-      if (!filter && value) return res.send(
-        mockUsers.filter((user) => user[filter].includes(value))
-      )
-      return res.send(mockUsers)  
+        ,(req, res) => {
+          console.log(req.sessionID)
+          req.sessionStore.get(req.sessionID, (err, sessionData) => {
+            if (err) {
+              console.log(err)
+              throw err
+            }
+            console.log(sessionData)
+          })
+          const result = validationResult(req)
+          console.log(result)
+          const { query: { filter, value } } = req
+        
+          if (!filter && value) return res.send(
+            mockUsers.filter((user) => user[filter].includes(value))
+          )
+          return res.send(mockUsers)  
 })
 
 router.get('/api/users/:id', resolveIndexByUserId, (req, res) => {
