67ace24b7c9432a403ab7379ea0154ed02bc674b
Quentin Janvier <quentin.janvier@hotmail.fr>
2025-09-08 16:41:15 +0200

    first test user complete

--- src/__tests__/users.test.js
+++ src/__tests__/users.test.js
@@ -1,5 +1,6 @@
 const { getUserByIdHandler } = require('../handlers/users')
 
+
 const mockRequest = {
     findUserIndex: 1
 }
@@ -12,5 +13,16 @@ const mockResponse = {
 describe("get users", () => {
     it("should get user by id", () => {
         getUserByIdHandler(mockRequest, mockResponse)
+        expect(mockResponse.send).toHaveBeenCalled()
+        expect(mockResponse.send).toHaveBeenCalledWith({
+            id: 2, username: 'Jane Doe', displayName: 'Jane', password:'hello124'
+        })
+        expect(mockResponse.send).toHaveBeenCalledTimes(1)
+    })
+
+    it('should call sendStatus 404 if user not found', () => {
+        const copyMockRequest = { ...mockRequest, findUserIndex: 100 }
+        getUserByIdHandler(copyMockRequest, mockResponse)
+        expect(mockResponse.sendStatus).toHaveBeenCalled()
     })
 })
